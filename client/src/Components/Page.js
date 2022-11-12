a42d5234b17724c2158f29539713918a530f1fd5
JackPeplinski <jackEpeplinski@gmail.com>
2022-11-12 15:52:50 -0500

    Feat: Create page styling and checkmark

---
+++ client/src/Components/Page.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Louis Vuitton Purse</h1>
+      <h3 style={{ textAlign: "center" }}>Verified Luxury Goods</h3>
+      <h3>Owner</h3>
+      <p>0xNinjuhL</p>
+      <h3>Description</h3>
+      <p>
+        Show your loyalty. A HAPEBADGE gives you early access to merchandise,
+        events and more. Hold it close and stay HAPE.
+      </p>
+    </>
+  );
+};
+
+export default Page;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import Checkmark from "./Components/Checkmark";
+import Page from "./Components/Page";
+import Wrapper from "./Components/Wrapper";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Wrapper>
+      {/* <Page /> */}
+      <Checkmark />
+    </Wrapper>
   </React.StrictMode>
 );
