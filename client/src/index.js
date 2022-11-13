46047254e70231a684614c859553c0a620bea50d
Keegan Mongru <kmongru@uwo.ca>
2022-11-13 01:25:21 -0500

    move NFT API requests to single express endpoint

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import Checkmark from "./Components/Checkmark";
-import Page from "./Components/Page";
-import Wrapper from "./Components/Wrapper";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import Checkmark from './components/Checkmark';
+import Page from './components/Page';
+import Wrapper from './components/Wrapper';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Wrapper>
