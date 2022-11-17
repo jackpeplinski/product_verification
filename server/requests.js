ae75a71e99a41a81afe583e64686b093936071e8
JackPeplinski <jackEpeplinski@gmail.com>
2022-11-17 13:42:26 -0500

    Feat: Test push

--- server/requests.js
+++ server/requests.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const authorization = '45c480d6-842e-42a3-9258-63a3efa6d05d';
+const authorization = "45c480d6-842e-42a3-9258-63a3efa6d05d";
 
 // map walletAddress : clientName
 let mockDB = {
-  '0x964937a0afdd89569c87c582f83e881c1b036486': 'Verfied Seller A',
+  "0x964937a0afdd89569c87c582f83e881c1b036486": "Verfied Seller A",
 };
 
 const authorizeTag = async (q_params) => {
@@ -15,7 +15,7 @@ const authorizeTag = async (q_params) => {
   let { xuid, response } = res.data;
 
   let isVerified = false;
-  response == 'Pass' ? (isVerified = true) : null;
+  response == "Pass" ? (isVerified = true) : null;
 
   // this will determine what page to land on
   return isVerified;
@@ -26,9 +26,9 @@ const getNFTMetadata = async (contract_address, token_id) => {
   let res = await axios.get(
     `https://api.nftport.xyz/v0/nfts/${contract_address}/${token_id}`,
     {
-      params: { chain: 'ethereum' },
+      params: { chain: "ethereum" },
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: authorization,
       },
     }
@@ -53,10 +53,10 @@ const getTransactionDetails = async (contract_address, token_id) => {
   let res = await axios.get(
     `https://api.nftport.xyz/v0/transactions/nfts/${contract_address}/${token_id}`,
     {
-      params: { chain: 'ethereum', type: 'transfer' },
+      params: { chain: "ethereum", type: "transfer" },
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: '45c480d6-842e-42a3-9258-63a3efa6d05d',
+        "Content-Type": "application/json",
+        Authorization: "45c480d6-842e-42a3-9258-63a3efa6d05d",
       },
     }
   );
