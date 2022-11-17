ea9459304d6bf4c076608c7b3f983ac011dd12db
JackPeplinski <jackEpeplinski@gmail.com>
2022-11-17 16:39:17 -0500

    k

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 export const app = express();
 app.use(cors());
 app.use(express.json()); //allow use to parse body of req
-app.use(express.static(path.join(__dirname, "/../client/build")));
+// app.use(express.static(path.join(__dirname, "/../client/build")));
 
 const port = process.env.PORT || 3002;
 
