244c6c141205137111e379187108dd4f9e535f0f
Julia <jgroza24@gmail.com>
2022-11-16 23:21:02 -0500

    parsing URL and passing params to back-end

--- client/src/Components/Loading.js
+++ client/src/Components/Loading.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Loading = () => {
+  const params = (new URL(document.location)).searchParams;
+
+  const getValidation = async () => {
+    const res = await axios(options);
+    axios.get('/auth', params)
+    console.log(res);
+
+  };
   return <>Loading...</>;
 };
 
