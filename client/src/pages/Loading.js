84668d06e6e0b71e27ea9fb870748f300b86a472
JackPeplinski <jackEpeplinski@gmail.com>
2022-11-18 14:13:34 -0500

    Feat: Mock calls for testing

--- client/src/pages/Loading.js
+++ client/src/pages/Loading.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const contract_address_temp = '0x6fa9f4b50e2950a8137a76649193816fb29dad2c';
-const token_id_temp = '7981';
-const url = require('url');
+const contract_address_temp = "0x6fa9f4b50e2950a8137a76649193816fb29dad2c";
+const token_id_temp = "7981";
+const url = require("url");
 
 const Loading = () => {
   // // parse window here into base + query parameters
@@ -23,9 +23,15 @@ const Loading = () => {
     const cur_url = url.parse(window.location.href, true);
 
     // use params to call our backend + assign verified status
-    const res = await axios.get('https://vla-api.vercel.app/api/auth', {
-      params: cur_url.query,
-    });
+    // const res = await axios.get('https://vla-api.vercel.app/api/auth', {
+    //   params: cur_url.query,
+    // });
+    // setVerified(res.data);
+
+    // for testing
+    const res = await new Promise((res) =>
+      setTimeout(() => res({ data: true }), 2000)
+    );
     setVerified(res.data);
 
     // chain the call to get nft details
@@ -42,7 +48,22 @@ const Loading = () => {
     const url = `https://vla-api.vercel.app/api/details/${contract_address}/${token_id}`;
 
     // get request from express backend
-    const res = await axios.get(url);
+    // const res = await axios.get(url);
+
+    // For testing
+    const res = await new Promise((res) =>
+      setTimeout(
+        () =>
+          res({
+            data: {
+              details: "details",
+              owner_wallet: "owner_wallet",
+            },
+          }),
+        2000
+      )
+    );
+
     setNftData(res.data);
     setLoading(false);
   };
@@ -52,7 +73,7 @@ const Loading = () => {
       {isLoading ? (
         <h1>Loading...</h1>
       ) : isVerified ? (
-        navigate('/authenicated', { state: nftData })
+        navigate("/authenicated", { state: nftData })
       ) : (
         <h1>Error</h1>
       )}
